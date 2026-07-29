7312d569e6ab23df37fd671076cc9f15f8406230
Leo <leokkzzhzzz@users.noreply.github.com>
2026-07-29 09:49:01 +0800

    公司机器人更改与香港机器人更改合并

--- botbrain_ws/src/g1_pkg/scripts/replay_sequence_men.sh
+++ botbrain_ws/src/g1_pkg/scripts/replay_sequence_men.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+# 等待机器人稳定
 sleep 8
-ros2 service call /g1_robot/arm_cmd bot_custom_interfaces/srv/ArmCmd \
-    "{command: 6, name: '', names: ['go_1','go_2','go_3','go_4','go_5','go_6']}"
-
-ros2 service call /g1_robot/arm_cmd bot_custom_interfaces/srv/ArmCmd \
-    "{command: 6, name: '', names: ['back_1','back_2','back_3','back_4','back_5']}"
-
+# 顺序执行上前刷卡整套动作 door1~door6
+ros2 service call /g1_robot/arm_cmd bot_custom_interfaces/srv/ArmCmd     "{command: 6, name: '', names: ['door_1','door_2','door_3','door_4','door_5','door_6']}"
+# 顺序执行刷卡后收回复位动作 door_back1~door5
+ros2 service call /g1_robot/arm_cmd bot_custom_interfaces/srv/ArmCmd     "{command: 6, name: '', names: ['door_back_1','door_back_2','door_back_3','door_back_4','door_back_5']}"
+# 动作结束，手臂回到初始待机位
 ros2 service call /g1_robot/arm_cmd bot_custom_interfaces/srv/ArmCmd "{command: 3, name: ''}"
